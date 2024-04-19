8bce6d10e856920a4505b0d93a76022c7da745f9
Sr-rajabov <122497514+Sr-rajabov@users.noreply.github.com>
2024-04-19 19:25:09 +0300

    Battlefield.cpp

--- Battlefield.cpp
+++ Battlefield.cpp
@@ -6,31 +6,39 @@
 #include <utility>
 
 //Constructor & Destructor
-Battlefield::Battlefield(size_t rows, size_t cols) : rows(rows), cols(cols), field(new string *[rows]) {
-    *(field) = new string[rows * cols];
-    for (int i = 1; i != rows; ++i) {
-        *(field + i) = *(field + i - 1) + cols;
-    }
-    char count = '1';
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (i > 0 && i < rows - 1 && j != 0 && j != cols / 2 - 1 && j != cols - 1 && j != cols / 2) *(*(field + i) + j) = '~';
-            else if (j == cols / 2 - 1) *(*(field + i) + j) = '|';
-            if (j == cols - 1) *(*(field + i) + j) = ' ';
-            if (i == rows - 1) *(*(field + i) + j) = ' ';
+Battlefield::Battlefield(){
+    try {
+        field = new string *[rows];
+        *(field) = new string[rows * cols];
+        for (int i = 1; i != rows; ++i) {
+            *(field + i) = *(field + i - 1) + cols;
         }
+        char count = '1';
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (i > 0 && i < rows - 1 && j != 0 && j != cols / 2 - 1 && j != cols - 1 && j != cols / 2)
+                    *(*(field + i) + j) = '~';
+                else if (j == cols / 2 - 1) *(*(field + i) + j) = '|';
+                if (j == cols - 1) *(*(field + i) + j) = ' ';
+                if (i == rows - 1) *(*(field + i) + j) = ' ';
+            }
+        }
+        for (int i = 1; i < rows - 1; ++i, count++) {
+            *(*(field + i)) = count;
+            *(*(field) + i) = count;
+            *(*(field + i) + rows) = count;
+            *(*(field) + rows + i) = count;
+        }
+        *(*(field + rows - 2)) = "10";
+        *(*(field) + rows - 2) = "10";
+        *(*(field) + cols - 2) = "10";
+        *(*(field + rows - 2) + rows) = "10";
     }
-    for (int i = 1; i < rows - 1; ++i, count++) {
-        *(*(field + i)) = count;
-        *(*(field) + i) = count;
-        *(*(field + i) + rows) = count;
-        *(*(field) + rows + i) = count;
+    catch (bad_alloc const &ex) {
+        cerr << ex.what() << newline;
     }
-    *(*(field + rows - 2)) = "10";
-    *(*(field) + rows - 2) = "10";
-    *(*(field) + cols - 2) = "10";
-    *(*(field + rows - 2) + rows) = "10";
 }
+
 
 Battlefield::~Battlefield() {
     delete []*(field);
